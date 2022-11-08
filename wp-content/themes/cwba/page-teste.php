c686fe085f52e7433c7b8d5b33b7307cff3840a0
evertonmessias <everton.messias@gmail.com>
2022-11-08 15:30:12 +0000

    08-11-2022_15:30:12

--- wp-content/themes/cwba/page-teste.php
+++ wp-content/themes/cwba/page-teste.php
@@ -1,37 +1,72 @@
 <?php
-echo "TESTESSSS";
+
+echo "TESTEEEEE - CUIDADO !";
+
 /*
-$id1 = 373;
-$id2 = 374;
-$id3 = 375;
-$id4 = 376;
-$id5 = 377;
-$id6 = 378;
 
-$titulo1 = 'Aula 2 - Inserindo recursos dinâmicos';
-$titulo2 = 'Aula 3 - Gerenciando temas';
-$titulo3 = 'Aula 4 - Personalizar temas';
-$titulo4 = 'Aula 5 - Plugins';
-$titulo5 = 'Aula 6 - Editores especiais';
-$titulo6 = 'Aula 7 - Configurações e usuários';
+$id01 = 501;
+$id02 = 502;
+$id03 = 503;
+$id04 = 504;
+$id05 = 505;
+$id06 = 506;
+$id07 = 507;
+$id08 = 508;
+$id09 = 509;
+$id10 = 510;
+$id11 = 511;
+$id12 = 512;
+$id13 = 513;
+$id14 = 514;
 
-$slug1 = sanitize_title($titulo1);
-$slug2 = sanitize_title($titulo2);
-$slug3 = sanitize_title($titulo3);
-$slug4 = sanitize_title($titulo4);
-$slug5 = sanitize_title($titulo5);
-$slug6 = sanitize_title($titulo6);
+$titulo01 = 'Aula01 - Introdução';
+$titulo02 = 'Aula02 - Instalação de Plugins';
+$titulo03 = 'Aula03 - Como o Wordpress reconhece um Plugin';
+$titulo04 = 'Aula04 - Hooks';
+$titulo05 = 'Aula05 - Sub-menus e Menus';
+$titulo06 = 'Aula06 - Hooks avançados';
+$titulo07 = 'Aula07 - Guardando configurações';
+$titulo08 = 'Aula08 - Settings API';
+$titulo09 = 'Aula09 - Custom Posts e Taxonomias';
+$titulo10 = 'Aula10 - Metabox';
+$titulo11 = 'Aula11 - Dashboard Widgets';
+$titulo12 = 'Aula12 - Widgets';
+$titulo13 = 'Aula13 - ShortCode';
+$titulo14 = 'Aula14 - Acesso ao banco de dados';
+
+$slug01 = sanitize_title($titulo01);
+$slug02 = sanitize_title($titulo02);
+$slug03 = sanitize_title($titulo03);
+$slug04 = sanitize_title($titulo04);
+$slug05 = sanitize_title($titulo05);
+$slug06 = sanitize_title($titulo06);
+$slug07 = sanitize_title($titulo07);
+$slug08 = sanitize_title($titulo08);
+$slug09 = sanitize_title($titulo09);
+$slug10 = sanitize_title($titulo10);
+$slug11 = sanitize_title($titulo11);
+$slug12 = sanitize_title($titulo12);
+$slug13 = sanitize_title($titulo13);
+$slug14 = sanitize_title($titulo14);
 
 require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 
 global $wpdb;
 
 $sql = "INSERT INTO wp_posts values 
-($id1,'1','2022-11-07 12:00:00','2022-11-07 12:00:00','','$titulo1','','publish','open','open','','$slug1','','','2022-11-07 12:00:00','2022-11-07 12:00:00','','0','/?p=$id1','0','post','','0'),
-($id2,'1','2022-11-07 12:00:00','2022-11-07 12:00:00','','$titulo2','','publish','open','open','','$slug2','','','2022-11-07 12:00:00','2022-11-07 12:00:00','','0','/?p=$id2','0','post','','0'),
-($id3,'1','2022-11-07 12:00:00','2022-11-07 12:00:00','','$titulo3','','publish','open','open','','$slug3','','','2022-11-07 12:00:00','2022-11-07 12:00:00','','0','/?p=$id3','0','post','','0'),
-($id4,'1','2022-11-07 12:00:00','2022-11-07 12:00:00','','$titulo4','','publish','open','open','','$slug4','','','2022-11-07 12:00:00','2022-11-07 12:00:00','','0','/?p=$id4','0','post','','0'),
-($id5,'1','2022-11-07 12:00:00','2022-11-07 12:00:00','','$titulo5','','publish','open','open','','$slug5','','','2022-11-07 12:00:00','2022-11-07 12:00:00','','0','/?p=$id5','0','post','','0'),
-($id6,'1','2022-11-07 12:00:00','2022-11-07 12:00:00','','$titulo6','','publish','open','open','','$slug6','','','2022-11-07 12:00:00','2022-11-07 12:00:00','','0','/?p=$id6','0','post','','0')";
+($id01,'1','2022-11-07 13:01:00','2022-11-07 13:01:00','','$titulo01','','publish','open','open','','$slug01','','','2022-11-07 13:01:00','2022-11-07 13:01:00','','0','/?p=$id01','0','post','','0'),
+($id02,'1','2022-11-07 13:02:00','2022-11-07 13:02:00','','$titulo02','','publish','open','open','','$slug02','','','2022-11-07 13:02:00','2022-11-07 13:02:00','','0','/?p=$id02','0','post','','0'),
+($id03,'1','2022-11-07 13:03:00','2022-11-07 13:03:00','','$titulo03','','publish','open','open','','$slug03','','','2022-11-07 13:03:00','2022-11-07 13:03:00','','0','/?p=$id03','0','post','','0'),
+($id04,'1','2022-11-07 13:04:00','2022-11-07 13:04:00','','$titulo04','','publish','open','open','','$slug04','','','2022-11-07 13:04:00','2022-11-07 13:04:00','','0','/?p=$id04','0','post','','0'),
+($id05,'1','2022-11-07 13:05:00','2022-11-07 13:05:00','','$titulo05','','publish','open','open','','$slug05','','','2022-11-07 13:05:00','2022-11-07 13:05:00','','0','/?p=$id05','0','post','','0'),
+($id06,'1','2022-11-07 13:06:00','2022-11-07 13:06:00','','$titulo06','','publish','open','open','','$slug06','','','2022-11-07 13:06:00','2022-11-07 13:06:00','','0','/?p=$id06','0','post','','0'),
+($id07,'1','2022-11-07 13:07:00','2022-11-07 13:07:00','','$titulo07','','publish','open','open','','$slug07','','','2022-11-07 13:07:00','2022-11-07 13:07:00','','0','/?p=$id07','0','post','','0'),
+($id08,'1','2022-11-07 13:08:00','2022-11-07 13:08:00','','$titulo08','','publish','open','open','','$slug08','','','2022-11-07 13:08:00','2022-11-07 13:08:00','','0','/?p=$id08','0','post','','0'),
+($id09,'1','2022-11-07 13:09:00','2022-11-07 13:09:00','','$titulo09','','publish','open','open','','$slug09','','','2022-11-07 13:09:00','2022-11-07 13:09:00','','0','/?p=$id09','0','post','','0'),
+($id10,'1','2022-11-07 13:10:00','2022-11-07 13:10:00','','$titulo10','','publish','open','open','','$slug10','','','2022-11-07 13:10:00','2022-11-07 13:10:00','','0','/?p=$id10','0','post','','0'),
+($id11,'1','2022-11-07 13:11:00','2022-11-07 13:11:00','','$titulo11','','publish','open','open','','$slug11','','','2022-11-07 13:11:00','2022-11-07 13:11:00','','0','/?p=$id11','0','post','','0'),
+($id12,'1','2022-11-07 13:12:00','2022-11-07 13:12:00','','$titulo12','','publish','open','open','','$slug12','','','2022-11-07 13:12:00','2022-11-07 13:12:00','','0','/?p=$id12','0','post','','0'),
+($id13,'1','2022-11-07 13:13:00','2022-11-07 13:13:00','','$titulo13','','publish','open','open','','$slug13','','','2022-11-07 13:13:00','2022-11-07 13:13:00','','0','/?p=$id13','0','post','','0'),
+($id14,'1','2022-11-07 13:14:00','2022-11-07 13:14:00','','$titulo14','','publish','open','open','','$slug14','','','2022-11-07 13:14:00','2022-11-07 13:14:00','','0','/?p=$id14','0','post','','0')";
 
-dbDelta($sql);
+dbDelta($________sql);
